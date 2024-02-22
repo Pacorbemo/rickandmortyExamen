af711eff6ab58c97f52b0460770ed32df07904a1
pacorbemo <pacorbemo@gmail.com>
2024-02-22 12:22:45 +0100

    Primer deploy

---
+++ Readme.md
@@ -0,0 +1,1 @@
+Francisco Corbera Morales
---
+++ db/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const uri = "mongodb+srv://usuario:12345678abc@cluster0.ulerm98.mongodb.net/rickandmorty?retryWrites=true&w=majority"
+
+mongoose.connect(uri, {});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected to MongoDB")
+});
+
+
